75641df5f4fc9d2b914c18c82cd601dde7c870b1
Ctopham <craigmtopham@gmail.com>
2024-10-19 11:54:36 -0400

    Adding backend starter

---
+++ JarSense/src/main/java/com/example/JarSense/JarSenseApplication.java
@@ -0,0 +1,13 @@
+package com.example.JarSense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarSenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarSenseApplication.class, args);
+	}
+
+}
---
+++ JarSense/src/test/java/com/example/JarSense/JarSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JarSense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarSenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
